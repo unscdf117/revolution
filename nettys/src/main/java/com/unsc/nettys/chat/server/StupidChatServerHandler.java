4f7ea8f9dc8d2e01486cb3504e2f11f0a8298a35
844421877@qq.com <jsjohn117>
2019-01-04 10:23:05 +0800

    修改了代码 致敬一下spring

--- nettys/src/main/java/com/unsc/nettys/chat/server/StupidChatServerHandler.java
+++ nettys/src/main/java/com/unsc/nettys/chat/server/StupidChatServerHandler.java
@@ -21,29 +21,26 @@ public class StupidChatServerHandler extends SimpleChannelInboundHandler<String>
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {  // (2)
         Channel incoming = ctx.channel();
-        for (Channel channel : channels) {
-            channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 加入\n");
-        }
+        channels.forEach(channel -> channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 加入\n"));
         channels.add(ctx.channel());
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {  // (3)
         Channel incoming = ctx.channel();
-        for (Channel channel : channels) {
-            channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 离开\n");
-        }
+        channels.forEach(channel -> channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 离开\n"));
         channels.remove(ctx.channel());
     }
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String s) throws Exception { // (4)
         Channel incoming = ctx.channel();
         for (Channel channel : channels) {
-            if (channel != incoming){
+            if (channel != incoming) {
                 channel.writeAndFlush("[" + incoming.remoteAddress() + "]" + s + "\n");
-            } else {
-                channel.writeAndFlush("[you]" + s + "\n");
+                //其实foreach c -> {... return;}也是可以的 只是这个continue致敬一下spring
+                continue;
             }
+            channel.writeAndFlush("[you]" + s + "\n");
         }
     }
 
